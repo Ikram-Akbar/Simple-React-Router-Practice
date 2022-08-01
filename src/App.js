cc233dea00eb149634376811cfe7787fea08d372
Ikram-Git-Coder <ikramakbar71@gmail.com>
2022-08-01 10:53:27 +0600

    declare the route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import Friends from "./pages/Friends/Friends";
+import Portfolio from "./pages/Portfolio/Portfolio";
+import NotFound from "./pages/NotFound/NotFound";
 
 function App() {
   return (
     <div className="App">
-    <h1>hello this is from my app function</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/friends" element={<Friends />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/NotFound/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <h1> 404 The page is not Available</h1>
+        </div>
+    );
+};
+
+export default NotFound;
